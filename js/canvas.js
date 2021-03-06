9b360eb4839047467830e237fbd080b3310c585c
Nick Currin <njcurrin@gmail.com>
2021-03-05 16:12:21 -0800

    Got the Turtle working with the buttons, and learned a lot more from Jeffrey

--- js/canvas.js
+++ js/canvas.js
@@ -1,47 +1,56 @@
-class Turtle {
+const turtle = {
   constructor(tempX, tempY, tempSize,canvas){
     this.write = false;
     this.pos = [tempX,tempY];
     this.size = tempSize;
     this.orientation = 0;
-    this.oldPOS = [];
     this.canvas = canvas;
-    this.numberOfCommands = {
-      commandOrder:[],
-      forward:[],
-      right:[],
-    }
-  }
+  },
   render(){
-    ctx.fillStyle = 'red'
-    ctx.beginPath();
-    ctx.arc(...this.pos,this.size,0,Math.PI*2,false);
-    ctx.fill();
-  }
+    turtleCtx.translate(...this.pos);
+    turtleCtx.fillStyle = 'red'
+    turtleCtx.fillRect(-5,-10,10,20);
+    turtleCtx.translate(-this.pos[0],-this.pos[1]);
+ //   this.needsRender = false
+  },
   
-  forward(steps = 1) {
-    if(this.numberOfCommands.forward[commandNumber]===undefined){
-      this.numberOfCommands.commandOrder[commandNumber] = [this._forward(steps)]
-    } 
-    commandNumber++;
-  }
+  forward(steps = 10) {
+    this.pos[0] += steps;
+  },
+
+  back(steps = 10) {
+    this.forward(-steps);
+  },
   
-  rotate(degrees){
+  rotate(degrees = 15){
+    turtleCtx.translate(...this.pos);
+    turtleCtx.rotate(degrees * Math.PI / 180)
+    turtleCtx.translate(-this.pos[0],-this.pos[1]);
+    this.orientation += degrees;
+   // this.needsRender = true
+  },
 
-  }
-
-  _forward(steps){
-    this.oldPOS[0] = this.pos;
-    
+  rotateLeft(degrees = 15){
+    this.rotate(-degrees);
   }
 }
 
-const can = document.querySelector('canvas')
+const graph = {
+  pos: undefined
+}
 
-can.width = 200
-can.height = 200
+const turtleCan = document.querySelector('#turtleCanvas');
+const lineCan = document.querySelector('#lineCanvas');
 
-const ctx = can.getContext('2d')
+const canvasSquare = 1000;
+
+turtleCan.width = canvasSquare;
+turtleCan.height = canvasSquare;
+lineCan.width = canvasSquare;
+lineCan.height = canvasSquare;
+
+const turtleCtx = turtleCan.getContext('2d');
+const lineCtx = lineCan.getContext('2d');
 
 
 let lastTime = 0
@@ -51,22 +60,57 @@ const frameRateInMillis =  1000 / 20 // 30 FPS
 
 let x = 0
 let y = 100
+const forwardButt = document.querySelector('.forward');
+forwardButt.addEventListener('click', () => {turtle.forward();});
+const backButt = document.querySelector('.back');
+backButt.addEventListener('click', () => {turtle.back();});
+const rotateButt = document.querySelector('.rotate');
+rotateButt.addEventListener('click', () => {turtle.rotate();});
+const rotateLeftButt = document.querySelector('.rotateLeft');
+//rotateLeftButt.addEventListener('click', () => {turtle.rotateLeft();});
 
-let turtle,commandNumber;
+rotateLeftButt.addEventListener('pointerdown', leftButtonDown);
+rotateLeftButt.addEventListener('pointerup', leftButtonUp);
+
+
+function leftButtonDown (event) {
+  console.log(event)
+  console.log('Down')
+ // rotateLeftButt.addEventListener('pointerup', leftButtonUp, {once:true});
+ // rotateLeftButt.removeEventListener('pointerdown', leftButtonDown)
+}
+
+function leftButtonUp(event) {
+  console.log("Up")
+}
+
 
 function setup(){
   window.requestAnimationFrame(loop);
-  turtle = new Turtle(50,50,8,ctx);
+  turtle.constructor(50,50,8,turtleCtx);
+  graph.pos = turtle.pos.slice(0)
 }
 
 function draw () {
-  ctx.fillStyle = "blue"
-  ctx.fillRect(0, 0, can.width, can.height)
-  commandNumber = 0;
+  turtleCtx.clearRect(0,0,turtleCan.width,turtleCan.height);
+  lineCtx.fillStyle = "blue"
+  lineCtx.fillRect(0, 0, turtleCan.width, turtleCan.height)
   turtle.render();
-  turtle.forward();
-  console.log(turtle.numberOfCommands.commandOrder)
-  turtle.forward(5)
+  
+  // TODO: Did the turtle POS change?
+  // If it did, then draw a line from oldPOS to turtlePOS
+
+  if (turtle.pos[0] !== graph.pos[0] || turtle.pos[1] !== graph.pos[1]) {
+
+    console.log("Turtle Position Has Changed!")
+
+    // TODO: draw line from graph.pos to turtle.pos
+
+    graph.pos = turtle.pos.slice(0)
+  }
+
+  //turtle.forward();
+  //console.log(turtle.numberOfCommands.commandOrder)
 	// // Move box
 	// x = (x + 1) % 200
   
@@ -74,8 +118,8 @@ function draw () {
   
 
 	// // Box
-	// ctx.fillStyle = "red"
-  // ctx.fillRect(x - 15, y - 15, 30, 30)
+	// turtleCtx.fillStyle = "red"
+  // turtleCtx.fillRect(x - 15, y - 15, 30, 30)
 
 }
 
@@ -90,7 +134,7 @@ function loop (time) { // Add the time parameter, and requestAnimation frame fil
   
   if (aggregatedTime > frameRateInMillis) { // Divide by 1000 to get FPS
   
-  	aggregatedTime = 0 // Reset aggregated time, it can also be carried over.
+  	aggregatedTime = 0 // Reset aggregated time, it turtleCan also be carried over.
   	
     // DRAW HERE!
     draw()
@@ -99,5 +143,7 @@ function loop (time) { // Add the time parameter, and requestAnimation frame fil
 
 	window.requestAnimationFrame(loop)
 }
+
+setup();
 
 
